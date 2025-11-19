36ad18623f789de9573885dbe4e61720c0304017
Witfor4321 <wit.fornalik@gmail.com>
2025-11-19 22:43:42 +0100

    fixing problems with previous commit
    some code doubled because of branch changes

--- RoboScribe.py
+++ RoboScribe.py
@@ -1,34 +1,3 @@
-<<<<<<< Updated upstream
-#pip install numpy Pillow svgwrite
-import numpy as np
-import cv2 as cv2
-from PIL import Image, ImageDraw, ImageFont
-
-def text_mode(output_image, text):
-    #przygotowanie obrazu
-    draw = ImageDraw.Draw(output_image)
-    w,h = output_image.size
-    
-    #czcionka
-    font = ImageFont.truetype("arial.ttf", size=100)
-
-    #pozycja startowa tekstu
-    x, y = 50, 100
-
-    # Kolor konturu i jego grubość
-    color = (255,255,255)
-    outline_color = (0,0,0)
-    stroke_width =4
-    draw.text((x, y), text, font=font, fill=color,stroke_width=stroke_width, stroke_fill=outline_color )
-
-
-
-h,w = (2000, 2000)
-img_text = Image.new('RGB', (w, h), color='white')
-
-text_mode(img_text, "ąęśćźżółabcdefghijklmnopqrstuwxyz")
-img_text.show()
-=======
 #pip install numpy Pillow svgwrite
 import numpy as np
 import cv2 as cv2
@@ -71,5 +40,4 @@ def text_mode(output_image, text,svg_filename="output.svg"):
 h,w = (2000, 2000)
 img_text = Image.new('RGB', (w, h), color='white')
 
-text_mode(img_text, "ąęśćźżółabcdefghijklmnopqrstuwxyz")
->>>>>>> Stashed changes
+text_mode(img_text, "aąęśćźżółabcdefghijklmnopqrstuwxyz")
