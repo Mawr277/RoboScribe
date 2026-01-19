0f813263ea4920c6ee5fb6a262dc992a86fa67f3
Witfor4321 <wit.fornalik@gmail.com>
2026-01-19 19:55:55 +0100

    Moved test script to tests folder

--- software/TextToSvg/TextToSvg_test.py
+++
@@ -1,9 +0,0 @@
-from TextToSvg.TextToSvg import text_mode_paths
-
-
-w = 1000
-h = 1000
-text = "abcdefghijk"
-
-text_mode_paths(w,h, text, svg_filename="output_path_arial.svg", font_name="Arial.ttf")
-text_mode_paths(w,h, text, svg_filename="output_path_times.svg", font_name="Times New Roman.ttf")
