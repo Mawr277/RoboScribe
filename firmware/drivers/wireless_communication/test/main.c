c189edc80a7808b49fd06dd7a7170537aff11fb8
Witfor4321 <wit.fornalik@gmail.com>
2026-04-30 22:58:36 +0200

    innitial commit

---
+++ firmware/drivers/wireless_communication/test/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+#include "SDcard.h"
+#include "wireless_communication.h"
+
+void app_main(void)
+{
+
+}
