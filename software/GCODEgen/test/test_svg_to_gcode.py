e0fd30dc179d53d86e1347811d4c30e400efb2be
Bartosz Chabros <bartekchabros2@gmail.com>
2026-05-20 19:34:08 +0200

    zaokrąglanie do jedności

--- software/GCODEgen/test/test_svg_to_gcode.py
+++ software/GCODEgen/test/test_svg_to_gcode.py
@@ -4,9 +4,11 @@ from software.GCODEgen.svg_to_gcode import convert_svg_to_gcode
 
 
 input_file = "software/GCODEgen/test/output_path_arial.svg" 
-output_file = "software/GCODEgen/test/plik7.gcode" 
-skala = 2.0
-rozdzielczosc = 0.5
+output_file = "software/GCODEgen/test/test_maciej.gcode" 
+skala = 1.0
+rozdzielczosc = 1.0
+
+
 
 
 convert_svg_to_gcode(
